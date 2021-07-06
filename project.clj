d4b5864a86220d313d6ffa96494373fa223a9455
Koji Takahashi <popoppo39@gmail.com>
2021-07-06 16:52:46 +0900

    Use leiningen to manage project

---
+++ project.clj
@@ -0,0 +1,17 @@
+(defproject clojure-spinners "0.1.0-SNAPSHOT"
+  :description "Various spinners for Clojure"
+  :url "https://github.com/popoppo/clojure-spinners"
+  :license {:name "EPL-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :deploy-repositories [["clojars" {:url "https://repo.clojars.org"
+                                    :username :env/clojars_username
+                                    :password :env/clojars_password
+                                    :sign-releases false}]]
+
+  :plugins [[lein-exec "0.3.7"]]
+
+  :src-paths ["src"]
+  :test-paths ["test"]
+
+  :repl-options {:init-ns clojure-spinners.spinner})
