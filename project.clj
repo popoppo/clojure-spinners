1a972db8d000ea5a4d4f36e8eba02ad6dedef401
Koji Takahashi <popoppo39@gmail.com>
2021-07-07 10:36:30 +0900

    Update lisence

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject clojure-spinners "0.1.0-SNAPSHOT"
+(defproject clojure-spinners "0.1.0"
   :description "Various spinners for Clojure"
   :url "https://github.com/popoppo/clojure-spinners"
-  :license {:name "EPL-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "MIT"
+            :url "https://choosealicense.com/licenses/mit"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :deploy-repositories [["clojars" {:url "https://repo.clojars.org"
                                     :username :env/clojars_username
